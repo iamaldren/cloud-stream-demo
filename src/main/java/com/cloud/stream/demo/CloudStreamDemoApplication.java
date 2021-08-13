17eaf13142aca1e263da062c7752e223e298f015
iamaldren <aldrenbobiles@gmail.com>
2021-08-13 15:26:44 +0800

    removed unused imports

--- src/main/java/com/cloud/stream/demo/CloudStreamDemoApplication.java
+++ src/main/java/com/cloud/stream/demo/CloudStreamDemoApplication.java
@@ -2,9 +2,6 @@ package com.cloud.stream.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.util.function.Consumer;
 
 @SpringBootApplication
 public class CloudStreamDemoApplication {
